7c330d07e8ef3cd2a21b5bbb5fc5710ebd5dea02
netmasterpro <102943190+netmasterpro@users.noreply.github.com>
2025-05-24 12:40:18 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,40 @@
+const CACHE_NAME = 'culebrita-v2';
+const ASSETS = [
+  "/",
+  "index.html",
+  "estilos.css",
+  "juego.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("culebrita-v1").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "estilos.css",
-        "juego.js",
-        "manifest.json",
-        "icon-192.png",
-        "icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    )).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+      .catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('index.html');
+        }
+      })
   );
 });
